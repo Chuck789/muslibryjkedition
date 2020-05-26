c568d354d4dc090fae2b136e0eec924b28090883
Chuck789 <64357215+Chuck789@users.noreply.github.com>
2020-05-26 17:17:48 +0200

    Add files via upload

---
+++ src/main/java/com/januszcorporation/muslibryjkedition/MuslibryjkeditionApplication.java
@@ -0,0 +1,13 @@
+package com.januszcorporation.muslibryjkedition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuslibryjkeditionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuslibryjkeditionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/januszcorporation/muslibryjkedition/MuslibryjkeditionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.januszcorporation.muslibryjkedition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuslibryjkeditionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
